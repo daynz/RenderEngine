4316eb6a931895d9fdd63a458743bf4606a389ad
shiroikami <1587402247@qq.com>
2024-09-14 16:25:32 +0800

    # 新增Texture类

---
+++ include/Texture.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class Texture
+{
+public:
+	Texture(std::string texturePath);
+
+private:
+	unsigned int texture;
+
+protected:
+
+};
---
+++ src/Texture.cpp
@@ -0,0 +1,35 @@
+#include "Texture.hpp"
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "iostream"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+Texture::Texture(std::string texturePath)
+{
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_set_flip_vertically_on_load(true);
+	int width, height, nrChannels;
+	const char* path = texturePath.c_str();
+	unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RPG, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Error::Texture::Failed_to_Load_Texture" << std::endl;
+	}
+	stbi_image_free(data);
+}
+
