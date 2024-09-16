b68cb4274c929d0a6639ac3069b44da40b461467
shiroikami <1587402247@qq.com>
2024-09-16 14:05:40 +0800

    # 暂存texture

--- include/Texture.hpp
+++ include/Texture.hpp
@@ -7,8 +7,13 @@ public:
 	Texture(std::string texturePath);
 
 	unsigned int texture()const;
+
 	std::string type()const;
+	void type(std::string t);
+	std::string path()const;
+	void path(std::string p);
 private:
+	std::string m_Path;
 	unsigned int m_Texture;
 	std::string m_type;
 protected:
